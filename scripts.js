44557c592110ef002477a31690996acbd497b9da
chatmane <chatmane10@gmail.com>
2017-07-17 13:13:53 -0500

    change

--- scripts.js
+++ scripts.js
@@ -6,3 +6,17 @@ function submitcontactform(){
         alert('Thank You For Submitting');
     }
 }
+
+$(document).ready(function() {  
+)};
+
+function submitcontactform(){
+    event.preventDefault();
+    var x = document.forms["contactForm"]["email"].value;
+    if (x == ""){
+        alert("Email must be filled out");
+    }else{
+        $("mycontactform").fadeout();
+        $("#success").fadein();
+    }
+}
